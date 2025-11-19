abcadbf5ac145e7b22871fc508d8006ebbca62b3
yerson.zip <yersonochoa.2005@gmail.com>
2025-11-19 13:13:33 -0500

    v9 deploy

--- powerHouse/wsgi.py
+++ powerHouse/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'powerHouse.settings')
 
+##Uno de los programadores fue sacrificado para que el deploy funcionara 
+
 application = get_wsgi_application()
