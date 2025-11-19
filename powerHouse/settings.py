0af9642c2922cf6fc06d59c0ad2f81daa1482ac6
yerson.zip <yersonochoa.2005@gmail.com>
2025-11-19 12:38:18 -0500

    v4 deploy

--- powerHouse/settings.py
+++ powerHouse/settings.py
@@ -135,4 +135,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'application' / 'static',
 ]
 
+# Configuración de WhiteNoise para servir archivos estáticos
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 ALLOWED_HOSTS =['*']
