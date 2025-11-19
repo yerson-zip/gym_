7ccfe73373292f124878c89c2b44ea923935aeed
yerson.zip <yersonochoa.2005@gmail.com>
2025-11-19 12:40:40 -0500

    v5 deploy

--- powerHouse/settings.py
+++ powerHouse/settings.py
@@ -131,6 +131,18 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# Internationalization
+LANGUAGE_CODE = 'es'
+TIME_ZONE = 'America/Bogota'
+USE_I18N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Directorios adicionales donde Django buscará archivos estáticos
 STATICFILES_DIRS = [
     BASE_DIR / 'application' / 'static',
 ]
